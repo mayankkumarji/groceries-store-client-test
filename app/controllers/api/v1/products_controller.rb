3bc0929c34eae1bbd5a00ed89f17b17a9bf0d374
Mayank <mkumar.ror@gmail.com>
2021-11-12 13:48:30 +0530

    Search api parameters documentation

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class ProductsController < ApplicationController
       # GET /api/v1/products
+      # To search product two parameters will be use.
+      # 1. `categories` must be comma seperated ids
+      # 2. `name` product name
       def index
         products = Product.search(params)
         render json: { data: ActiveModelSerializers::SerializableResource.new(products, each_serializer: ProductSerializer) }
